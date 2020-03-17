1fed622a6fa6c8a7eb56dfad7e10b51d6cfa1828
Alex Miller <alex.k.miller@gmail.com>
2020-03-17 09:37:49 -0400

    Class dictionary

--- extract_html.py
+++ extract_html.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import json
 from bs4 import BeautifulSoup
 
 
@@ -7,6 +8,9 @@ if os.path.exists("output.zip"):
     os.remove("output.zip")
 if os.path.isdir("output"):
     shutil.rmtree("output")
+
+
+class_dict = dict()
 
 
 build_dirs = {
@@ -35,6 +39,7 @@ ignore_dirs = [
 
 
 for parent_slug, root_dir in build_dirs.items():
+    class_dict[parent_slug] = dict()
     for dirname, dirs, files in os.walk(root_dir, followlinks=True):
         dir_split = dirname.split(os.path.sep)
         root_len = len(root_dir.split(os.path.sep))
@@ -47,6 +52,14 @@ for parent_slug, root_dir in build_dirs.items():
                     if main is None:
                         main = soup.find("div", attrs={"id": "main"})
                     if main is not None:
+                        for tag in main():
+                            if tag.name not in class_dict[parent_slug].keys():
+                                class_dict[parent_slug][tag.name] = dict()
+                            tag_class = tag.get("class", None)
+                            if tag_class:
+                                if "|".join(tag_class) not in class_dict[parent_slug][tag.name].keys():
+                                    class_dict[parent_slug][tag.name]["|".join(tag_class)] = 0
+                                class_dict[parent_slug][tag.name]["|".join(tag_class)] += 1
                         pre_spans = main.findAll("span", attrs={'class': 'pre'})
                         for pre_span in pre_spans:
                             pre_span.name = 'pre'
@@ -59,5 +72,8 @@ for parent_slug, root_dir in build_dirs.items():
                             os.makedirs(output_dir)
                         with open(output_path, 'w') as output_xml:
                             output_xml.write(str(main))
+
+with open("class_dict.json", "w") as json_file:
+    json.dump(class_dict, json_file, indent=4)
 
 shutil.make_archive("output", "zip", "output")
